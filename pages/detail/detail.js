88478c6d6bb52c1802fbb1d3fd91cf6307ef80a3
YINGFOX <jlc2580@163.com>
2020-04-04 21:50:19 +0800

    第一次提交

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -1,79 +1,67 @@
+// pages/detail/detail.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    // 当前选中的导航栏索引,默认是0
-    currentIndexNav: 0,
-    // 这是首页导航栏请求数据接收数组
-    navList: [],
-    //轮播图数据
-    swiperList: [],
-    // 视频列表数据
-    videosList: []
+    // 视频详情对象
+    videoInfo: null,
+    // 获取推荐视频
+    othersList: [],
+    // 评论对象
+    commentData: null
   },
-  //发送请求获取视频列表数据
-  getVideosList: function () {
+  // 根据ID获取视频详情
+  getCurrentVideo: function (videoId) {
     let that = this;
     wx.request({
-      url: 'https://www.fastmock.site/mock/0cc8353a68511aad722aa387610a4ce1/bili/videosList',
+      url: 'https://www.fastmock.site/mock/0cc8353a68511aad722aa387610a4ce1/bili/videoDetail?id=' + videoId,
       success: function (res) {
         if (res.data.code === 0) {
           that.setData({
-            videosList: res.data.data.videosList
+            videoInfo: res.data.videoInfo.video
           })
         }
       }
     })
   },
-  //发送请求获取轮播图数据
-  getSwiperList: function () {
+  // 获取推荐视频
+  getOtherList: function (videoId) {
     let that = this;
     wx.request({
-      url: 'https://www.fastmock.site/mock/0cc8353a68511aad722aa387610a4ce1/bili/swiperList',
+      url: 'https://www.fastmock.site/mock/0cc8353a68511aad722aa387610a4ce1/bili/othersList?id='+videoId,
       success: function (res) {
         if (res.data.code === 0) {
           that.setData({
-            swiperList: res.data.data.swiperList
+            othersList:res.data.data.othersList
           })
         }
       }
     })
   },
-  // 发送请求获取导航栏数据
-  getNavList: function () {
+  // 获取评论
+  getCommentList: function (videoId) {
     let that = this;
-    // 内置方法
     wx.request({
-      url: 'http://mock-api.com/mnEe4VnJ.mock/navList',
+      url: 'https://www.fastmock.site/mock/0cc8353a68511aad722aa387610a4ce1/bili/commentsList?id='+videoId,
       success: function (res) {
         if (res.data.code === 0) {
           that.setData({
-            navList: res.data.data.navList
+            commentData: res.data.data.commentData
           })
         }
       }
-    })
-  },
-  // 点击导航栏事件
-  activenav: function (e) {
-    this.setData({
-      //使用传递过来的参数
-      currentIndexNav: e.target.dataset.index
     })
   },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    // 1,调用函数请求导航栏数据
-    this.getNavList();
-    // 2,调用函数请求轮播图数据
-    this.getSwiperList();
-    // 3,调用函数请求视频列表数据
-    this.getVideosList();
-
+    let videoId = options.id;
+    this.getCurrentVideo(videoId)
+    this.getOtherList(videoId)
+    this.getCommentList(videoId)
   },
 
   /**
